00851f7b094cfef07d3e5a50126cccf0c919090d
Javohir Mamaniyazov <118096306+javohirmamaniyazov@users.noreply.github.com>
2023-08-02 15:34:14 +0500

    Update list.blade.php

--- resources/views/admin/product/list.blade.php
+++ resources/views/admin/product/list.blade.php
@@ -8,10 +8,10 @@
             <div class="container-fluid">
                 <div class="row mb-2 ml-1">
                     <div class="col-sm-6">
-                        <h1>Product List</h1>
+                        <h1>Mahsulotlar ro'yxati</h1>
                     </div>
                     <div class="col-sm-6" style="text-align: right">
-                        <a href="{{ url('admin/product/add') }}" class="btn btn-primary">Add new Product</a>
+                        <a href="{{ url('admin/product/add') }}" class="btn btn-primary"><i class="nav-icon fas fa-shopping-cart"></i><span> +</span></a>
                     </div>
                 </div>
             </div><!-- /.container-fluid -->
@@ -26,23 +26,21 @@
                         @include('_messages')
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Search Product</h3>
+                                <h3 class="card-title">Qidiruv</h3>
                             </div>
                             <form method="get" action="">
                                 <div class="card-body">
                                     <div class="row">
 
 
-                                        <div class="form-group col-md-3">
-                                            <label>Name</label>
-                                            <input type="text" name="name" value="{{ Request::get('name') }}"
-                                                class="form-control" placeholder="Name">
-                                        </div>
-                                        <div class="form-group col-md-3">
-                                            <button class="btn btn-primary" type="submit"
-                                                style="margin-top: 30px;">Search</button>
-                                            <a href="{{ url('admin/product/list') }}" class="btn btn-success"
-                                                style="margin-top: 30px;">Reset</a>
+                                        <div class="form-group col-md-12">
+                                            <label>Mahsulot Nomi</label>
+                                            <div class="d-flex">
+                                                <input type="text" name="name" value="{{ Request::get('name') }}"
+                                                class="form-control" placeholder="Mahsulot Nomi">
+                                                <a href="{{ url('admin/product/list') }}" class="btn" style="margin-left: -38px"><i class="fa fa-times"></i></a>
+                                                <button class="ml-1 btn btn-dark" type="submit"><i class="fa fa-search"></i></button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -55,19 +53,20 @@
                         <div class="card">
 
                             <div class="card-header">
-                                <h3 class="card-title">All Products</h3>
+                                <h3 class="card-title">Barcha Mahsulotlar</h3>
                             </div>
                             <!-- /.card-header -->
-                            <table id="productTable" class="table table-striped">
+                            <div class="card-body table-responsive p-0" style="overflow: auto">
+                            <table id="productTable" class="table table-tripled">
                                 <thead>
                                     <tr>
                                         <th>#</th>
-                                        <th>Product Name</th>
-                                        <th>Category Name</th>
-                                        <th>Created By</th>
-                                        <th>Image</th>
-                                        <th>Status</th>
-                                        <th>Action</th>
+                                        <th style="min-width: 131px;">Mahsulot Nomi</th>
+                                        <th style="min-width: 146px;">Kategoriya Nomi</th>
+                                        <th style="min-width: 169px;">Foydalanuvchi Nomi</th>
+                                        <th>Rasm</th>
+                                        <th>Holat</th>
+                                        <th>Harakat</th>
                                     </tr>
                                 </thead>
                                 <tbody style="cursor: pointer;">
@@ -89,19 +88,20 @@
                                                     Inactive
                                                 @endif
                                             </td>
-                                            <td>
+                                            <td style="min-width: 145px;">
                                                 <a href="{{ url('admin/product/show/' . $item->id) }}"
-                                                    class="btn btn-primary btn-sm"><i class="fas fa-solid fa-eye"></i></a>
+                                                    class="btn btn-primary btn-sm" style="width: 34px;"><i class="fas fa-solid fa-eye"></i></a>
                                                 <a href="{{ url('admin/product/edit/' . $item->id) }}"
-                                                    class="btn btn-warning btn-sm"><i class="fas fa-solid fa-pen"></i></a>
+                                                    class="btn btn-warning  btn-sm" style="width: 34px;"><i class="fas fa-solid fa-pen"></i></a>
                                                 <a href="{{ url('admin/product/delete/' . $item->id) }}"
-                                                    class="btn btn-danger btn-sm"><i class="fa fa-trash"
+                                                    class="btn btn-danger btn-sm" style="width: 34px;"><i class="fa fa-trash"
                                                         aria-hidden="true"></i></a>
                                             </td>
                                         </tr>
                                     @endforeach
                                 </tbody>
                             </table>
+                            </div>
                             <!-- /.card-body -->
                         </div>
                         <!-- /.card -->
@@ -117,31 +117,35 @@
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             var tableBody = document.getElementById('productTable').querySelector('tbody');
-    
+
             // Get the current row order from localStorage (if available)
             var savedOrder = localStorage.getItem('productTable_order');
             if (savedOrder) {
                 savedOrder = JSON.parse(savedOrder);
             }
-    
+
             // Update the table with the new row order
             updateTableOrder(savedOrder);
-    
+
             // Initialize Sortable.js on the table to make the rows sortable
             var sortableTable = new Sortable(tableBody, {
-                animation: 150, // Set the animation speed in milliseconds (optional)
+                animation: 150,
+                handle: '.fa-bars',// Set the animation speed in milliseconds (optional)
                 onEnd: function (evt) {
                     // This function is triggered when the user stops dragging a row
                     // Use the 'evt' parameter to access the updated row order
                     var updatedOrder = Array.from(tableBody.children).map(function (row) {
                         return row.dataset.id;
                     });
-    
+
                     // Save the updated row order to localStorage
                     localStorage.setItem('productTable_order', JSON.stringify(updatedOrder));
+
+                    // Output a success message
+                    showSuccessMessage("Muvaffaqiyatli amalga oshirildi");
                 }
             });
-    
+
             // Function to update the table rows order
             function updateTableOrder(order) {
                 if (!order) {
@@ -150,10 +154,10 @@
                         return row.dataset.id;
                     });
                 }
-    
+
                 var rows = Array.from(tableBody.children);
                 var newRowOrder = [];
-    
+
                 order.forEach(function (id) {
                     var row = rows.find(function (row) {
                         return row.dataset.id === id;
@@ -162,16 +166,31 @@
                         newRowOrder.push(row);
                     }
                 });
-    
+
                 // Remove all existing rows from the table body
                 while (tableBody.firstChild) {
                     tableBody.removeChild(tableBody.firstChild);
                 }
-    
+
                 // Append the rows back to the table body in the new order
                 newRowOrder.forEach(function (row) {
                     tableBody.appendChild(row);
                 });
+            }
+
+            // Function to show a success message
+            function showSuccessMessage(message) {
+                var messageDiv = document.createElement('div');
+                messageDiv.classList.add('alert', 'alert-success');
+                messageDiv.innerHTML = message;
+
+                var container = document.querySelector('.content-wrapper');
+                container.insertBefore(messageDiv, container.firstChild);
+
+                // Automatically remove the message after 3 seconds
+                setTimeout(function () {
+                    container.removeChild(messageDiv);
+                }, 4000);
             }
         });
     </script>
